350baba6b5f36949174a41da4fc225aced3237a5
Sn1per <egorveremovsky@yandex.ru>
2024-08-01 02:25:29 +0300

    second commit

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
-cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
+def parse_recipes(file_path):
+  cook_book = {}
+
+  with open(file_path, 'r', encoding='utf-8') as file:
+      lines = file.readlines()
+
+      i = 0
+      while i < len(lines):
+          dish_name = lines[i].strip()
+          if not dish_name:  # Пропускаем пустые строки
+              i += 1
+              continue
+
+          i += 1
+
+          # Проверка, что следующая строка содержит число
+          if i < len(lines):
+              try:
+                  quantity = int(lines[i].strip())
+              except ValueError:
+                  print(f"Ошибка: Ожидалось число, но получено '{lines[i].strip()}' вместо количества ингредиентов для '{dish_name}'.")
+                  break
+
+          i += 1
+
+          ingredients = []
+          for _ in range(quantity):
+              if i < len(lines):
+                  ingredient_info = lines[i].strip().split(' | ')
+                  if len(ingredient_info) != 3:
+                      print(f"Ошибка: Неверный формат ингредиента '{lines[i].strip()}'.")
+                      break
+                  ingredient_name = ingredient_info[0]
+                  ingredient_quantity = int(ingredient_info[1])
+                  ingredient_measure = ingredient_info[2]
+                  ingredients.append({
+                      'ingredient_name': ingredient_name,
+                      'quantity': ingredient_quantity,
+                      'measure': ingredient_measure
+                  })
+                  i += 1
+
+          cook_book[dish_name] = ingredients
+
+  return cook_book
+
+# Укажите путь к вашему файлу
+file_path = 'recipes.txt'
+cook_book = parse_recipes(file_path)
+
+
+
+
+
+
 def get_shop_list_by_dishes(dishes, person_count):
   shop_list = {}
   for dish in dishes:
@@ -25,5 +62,3 @@ def get_shop_list_by_dishes(dishes, person_count):
       shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
   return shop_list
 print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
-
-
